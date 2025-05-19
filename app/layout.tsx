df7ed59cf5b3fa72115667162e9ac7bc4c896105
karlovkar <spravca@karlovaves.sk>
2025-05-19 18:22:23 +0200

    Add favicon assets

--- app/layout.tsx
+++ app/layout.tsx
@@ -24,6 +24,24 @@ export default function RootLayout({
           href="https://api.mapbox.com/mapbox-gl-js/v3.11.1/mapbox-gl.css"
           rel="stylesheet"
         />
+        <link rel="icon" href="/favicon.ico" />
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/favicon/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon/favicon-16x16.png"
+        />
       </head>
       <body className="antialiased">{children}</body>
     </html>
